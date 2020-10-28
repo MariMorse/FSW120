51cffbf2edc9f069c42afee63599fc4dcd456df6
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-10-28 13:52:23 -0400

    commit

---
+++ week1/TodoPart1/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Todo item 1</p>
+
+            <input type="checkbox" />
+            <p>Todo item 2</p>
+            
+            <input type="checkbox" />
+            <p>Todo item 3</p>
+            
+            <input type="checkbox" />
+            <p>Todo item 4</p>
+        </div>
+    )
+}
+
+export default App
