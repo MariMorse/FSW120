369e2581da38692477057aed1ab8117981a02b3c
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-11-07 10:39:07 -0500

    commit

--- week2-project1/src/App.js
+++ week2-project1/src/App.js
@@ -1,10 +1,8 @@
-import React from "react";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-
-function App() {
-  return (
-    <div>Hello World
-    </div>
-  );
-}
-export default App; 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
