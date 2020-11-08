efcb1b597dc1ed2e6ceb809bd63b5ddf49c04b43
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-11-08 12:53:42 -0500

    commit

--- week3-assignment1/src/App.js
+++ week3-assignment1/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data.json'
+import SuperHero from './SuperHero'
 
 function App() {
+  const superHero = data.map(hero => <SuperHero name={hero.name} show={hero.show} imageName={hero.imageName} catchPhrase={hero.catchPhrase}/>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {superHero}
     </div>
   );
 }
