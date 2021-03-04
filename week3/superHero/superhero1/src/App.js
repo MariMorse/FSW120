f6927737561ddb3ecd4ebc487cb4ea4be22e7828
Mari Morse <marianne.morse@bryanuniversity.edu>
2021-03-03 19:55:31 -0500

    commit

---
+++ week3/superHero/superhero1/src/App.js
@@ -0,0 +1,13 @@
+import data from './data.json'
+import SuperHero from './SuperHero'
+
+function App() {
+  const superHero = data.map(hero => <SuperHero name={hero.name} show={hero.show} imageName={hero.imageName} catchPhrase={hero.catchPhrase}/>)
+  return (
+    <div className="App">
+      {superHero}
+    </div>
+  );
+}
+
+export default App;
