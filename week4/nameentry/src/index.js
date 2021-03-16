d287165794c0bed923deacc07790d4d1a002da0a
MariMorse <marianne.morse@bryanuniversity.edu>
2021-03-15 22:35:55 -0400

    ommit

--- week4/nameentry/src/index.js
+++ week4/nameentry/src/index.js
@@ -1,6 +1,6 @@
 import App from "./App";
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 
-ReactDOM.render( < App / > , document.getElementById('root'))
+ReactDOM.render( < App / > , document.getElementById("root"))
