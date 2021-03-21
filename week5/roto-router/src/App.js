5552cc840acb1c2337fd14f6b76892ec1f168bb0
MariMorse <marianne.morse@bryanuniversity.edu>
2021-03-21 13:46:08 -0400

    commit

--- week5/roto-router/src/App.js
+++ week5/roto-router/src/App.js
@@ -1,8 +1,23 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 function App() {
     return (
-      <div>Code goes here</div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/services">Services</Link>
+              </li>
+          </ul>
+        </nav>
+      </div>
     )
 }
 
