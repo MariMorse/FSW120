504020a7dc36ed4a3bfdceb030905ee1f87206ea
MariMorse <marianne.morse@bryanuniversity.edu>
2021-03-21 13:05:50 -0400

    commit

--- week5/roto-router/src/index.js
+++ week5/roto-router/src/index.js
@@ -4,4 +4,8 @@ import {BrowserRouter as Router} from "react-router-dom"
 
 import App from "./App"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(
+    <Router>
+      <App />
+    </Router>,
+    document.getElementById("root"))
