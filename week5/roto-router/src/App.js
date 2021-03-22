bfb69ea34e3ab3aa1e60c43dca802b8e1695ca0d
MariMorse <marianne.morse@bryanuniversity.edu>
2021-03-22 14:21:07 -0400

    commit

--- week5/roto-router/src/App.js
+++ week5/roto-router/src/App.js
@@ -1,16 +1,29 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import {Link, Switch, Route} from "react-router-dom"
+
+import Home from "./Home"
+import About from "./About"
+import Services from "./Services"
 
 function App() {
     return (
       <div>
         <nav>
               <Link to="/">Home</Link>
+              <hr />
               <Link to="/about">About</Link>
+              <hr />
               <Link to="/services">Services</Link>      
         </nav>
+
+
+          <Switch>
+              <Route  exact path="/"><Home /></Route>
+              <Route path="/about"><About /></Route>
+              <Route path="/services"><Services /></Route>
+          </Switch>
         <header>
-          <h1>Down the Drain Plumbing</h1>
+          
           </header>
       </div>
     )
---
+++ week5/roto-router/src/Home.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Home() {
+    return (
+        <h1>Down the Drain Plumbing</h1>
+    )
+}
+
+export default Home
