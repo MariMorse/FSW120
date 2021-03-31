d16e5dad35d7bc39068881007b141e4da3c9fa3d
MariMorse <marianne.morse@bryanuniversity.edu>
2021-03-31 13:54:05 -0400

    commit

--- week5/roto-router/src/pages/Home.js
+++ week5/roto-router/src/pages/Home.js
@@ -4,6 +4,11 @@ function Home() {
     return (
         <div style={{display:"flex", backgroundColor:"lightskyblue"}}>
             <img src='https://www.pinterest.com.au/pin/619245017490547595/' alt="RotoRouter" style={{width:"10px"}} />
+            <div style={{width:"100%", textAlign:"center", paddingTop:"40px"}}>
+                <h1>Welcome to Down the Drain Plumbing</h1>
+                <h3 style={{fontWeight:"bold"}}>Call 614-567-5309 to Schedule Now</h3>
+                <p>Down the Drain Plumbing is here to solve your plumbing problems quickly and professionaly. We are dependable and reliable, and we are available 24/7 to take care of all your plumbing needs.<br />We take care of various plumbing jobs for residential and commercial properties. Down the Drain Plumbing is a full service plumbing company, locally owned and operated.</p>
+            </div>
         </div>
     )
 }
