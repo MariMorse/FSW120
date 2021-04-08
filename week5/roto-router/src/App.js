19fe68580de6f30860e320e67d537c9c19fd0c50
MariMorse <marianne.morse@bryanuniversity.edu>
2021-04-08 12:52:33 -0400

    commit

--- week5/roto-router/src/App.js
+++ week5/roto-router/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { Home, About, Services } from './pages'
+
 
 function App() {
   return (
     <div>
-      Down the Drain Plumbing
+      <h1>Down the Drain Plumbing</h1>
+      <Home />
+      <About />
+      <Services />
     </div>
   );
 }
