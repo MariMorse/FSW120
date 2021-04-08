d3bdd962fc0d22477818031af07b42d40ae4a6f1
MariMorse <marianne.morse@bryanuniversity.edu>
2021-04-07 21:21:45 -0400

    commit

--- week7/twitterapp/src/App.js
+++ week7/twitterapp/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Feed from "./Feed";
 import Sidebar from "./Sidebar";
-import Widget from "./Widget";
+import Widgets from "./Widgets";
 
 function App() {
   return (
